f26e6866a21006484b3299e01fd440f429ad9278
mdmahbuburrahman214-pixel <mdmahbuburrahman214@gmail.com>
2026-07-01 01:01:22 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,3 +18,7 @@ echo "Downloading AI Models..."
 python download_models.py
 
 echo "Installation Complete."
+git clone https://github.com/KwaiVGI/LivePortrait.git
+cd LivePortrait
+pip install -r requirements.txt
+cd ..
